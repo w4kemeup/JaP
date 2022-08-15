d7d836abfbdc0fe81628c67ecf8b6a59eef18d77
w4kemeup <110257712+w4kemeup@users.noreply.github.com>
2022-08-15 14:35:28 -0300

    ajustes a estilos

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ let password = document.getElementById("password");
 
 
 // funciones para modificar el borde del cuadro de texto tanto para el usuario como para contraseña, y se indica que se complete el cuadro de texto necesario //
-// al ingresar un dato en el cuadro, el mismo cambiara su borde a color verde e ira acompañado de un tick //
+// al ingresar un dato en el cuadro, el mismo cambiará su borde a color verde e irá acompañado de un tick //
 
 function usuarioBien() {
     usuario.setAttribute("style", "border: 0.5px solid green") + 
@@ -15,7 +15,7 @@ function passwordBien() {
     (document.getElementById("correctoPassword").style.visibility = "visible")
 }
 
-// al tener un cuadro vacio, su borde cambiara a color rojo y sera acompañado de un icono de alerta //
+// al tener un cuadro vacío, su borde cambiará a color rojo y será acompañado de un icono de alerta //
 
 function usuarioMal() {
     usuario.setAttribute("style", "border: 0.5px solid red") +
@@ -31,17 +31,18 @@ function passwordMal() {
 }
 
 
-// al hacer click en el boton de id lgnBtn, se agrega una escucha de evento click, que comprueba si las condiciones son las indicadas, se aplicara el estilo correspondiente segun la situacion y en caso de estar todo correcto, se procede a entrar al sitio//
+// al hacer click en el botón de id lgnBtn, se agrega una escucha de evento click, que comprueba si las condiciones son las indicadas, se aplicará //
+// el estilo correspondiente según la situación y en caso de estar todo correcto, se procede a entrar al sitio//
 
 document.getElementById("lgnBtn").addEventListener("click", function () {
-
+        
     if (usuario.value === "" && password.value === "") {
-        (usuarioMal() + passwordMal());
+        (usuarioMal() + passwordMal()) ;
     } else if (usuario.value.length >= 1 && password.value === "") {
         (usuarioBien() + passwordMal());
     } else if (usuario.value === "" && password.value.length >= 1) {
         (usuarioMal() + passwordBien());
     } else if (usuario.value.length >= 1 && password.value.length >= 1) {
-        (usuarioBien() + passwordBien() + location.replace("https://w4kemeup.github.io/JaP/portada.html"));
+        (usuarioBien() + passwordBien() + location.replace("portada.html"));
     }
 })
