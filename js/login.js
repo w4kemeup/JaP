dc89ae45358b92532c8313c06d69d838e01831ab
w4kemeup <110257712+w4kemeup@users.noreply.github.com>
2022-08-18 14:54:54 -0300

    modo oscuro con sus correspondientes estilos y js

--- js/login.js
+++ js/login.js
@@ -6,13 +6,13 @@ let password = document.getElementById("password");
 // al ingresar un dato en el cuadro, el mismo cambiará su borde a color verde e irá acompañado de un tick //
 
 function usuarioBien() {
-    usuario.setAttribute("style", "border: 0.5px solid green") + 
-    (document.getElementById("correctoUser").style.visibility = "visible")
-    
+    usuario.setAttribute("style", "border: 0.5px solid green") +
+        (document.getElementById("correctoUser").style.visibility = "visible")
+
 }
 function passwordBien() {
     password.setAttribute("style", "border: 0.5px solid green") +
-    (document.getElementById("correctoPassword").style.visibility = "visible")
+        (document.getElementById("correctoPassword").style.visibility = "visible")
 }
 
 // al tener un cuadro vacío, su borde cambiará a color rojo y será acompañado de un icono de alerta //
@@ -35,9 +35,9 @@ function passwordMal() {
 // el estilo correspondiente según la situación y en caso de estar todo correcto, se procede a entrar al sitio//
 
 document.getElementById("lgnBtn").addEventListener("click", function () {
-        
+
     if (usuario.value === "" && password.value === "") {
-        (usuarioMal() + passwordMal()) ;
+        (usuarioMal() + passwordMal());
     } else if (usuario.value.length >= 1 && password.value === "") {
         (usuarioBien() + passwordMal());
     } else if (usuario.value === "" && password.value.length >= 1) {
@@ -46,3 +46,24 @@ document.getElementById("lgnBtn").addEventListener("click", function () {
         (usuarioBien() + passwordBien() + location.replace("portada.html"));
     }
 })
+
+// funcion para modo oscuro //
+
+
+let boton = document.getElementById("botonOscuro"); // obtengo el boton con la id botonOscuro //
+let fondo = document.body; // establezco la variable fondo para agregarle el modo oscuro cada vez que presiono el boton // 
+
+boton.addEventListener("click", function (){ 
+    let tema = fondo.classList.toggle("oscuro")  // al body le agrego la clase "oscuro" //
+    localStorage.setItem("modo-oscuro", tema)   // y lo almaceno en una variable //
+})
+
+// mantener modo aunque se refresque la pagina // 
+
+let dark = localStorage.getItem("modo-oscuro");  // para obtener el valor del dato almacenado //
+
+if (dark=="true") {
+    fondo.classList.add("oscuro") 
+}else{
+    fondo.classList.remove("oscuro")
+}
