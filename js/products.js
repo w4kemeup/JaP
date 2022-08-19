2f782f6bedb93009047b70ff06850defba3b577d
w4kemeup <110257712+w4kemeup@users.noreply.github.com>
2022-08-19 17:12:24 -0300

    update en codigo products

--- js/products.js
+++ js/products.js
@@ -1,31 +1,43 @@
 const AUTOS = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+let listadoAutos = [];
 
-fetch(AUTOS)
-    .then(respuesta => respuesta.json())
-    .then(data => {
-        console.log(data)
 
-        for (let auto of data.products) {
-            let htmlContentToAppend = "";
-            htmlContentToAppend += `
-            <div class="list-group-item list-group-item-action">
+function mostrarListadoAutos() {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < listadoAutos.products.length; i++) {
+        let auto = listadoAutos.products[i];
+        htmlContentToAppend += `
+        <div onclick="setCatID(${auto.id})" class="list-group-item list-group-item-action cursor-active">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + auto.image + `" alt="product image" class="img-thumbnail">
+                    <img src="${auto.image}" alt="${auto.description}" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4 style="text-align:left">`+ auto.name + " " + "-" + " " + auto.currency + " " + auto.cost + `</h4> 
-                        <p> `+ auto.description + `</p> 
-                        </div>
-                        <small class="text-muted">` + auto.soldCount + ` vendidos</small> 
+                        <h4 class="mb-1">${auto.name} - ${auto.currency} ${auto.cost}</h4>
+                        <small class="text-muted">${auto.soldCount} vendidos</small>
                     </div>
-
+                    <p class="mb-1">${auto.description}</p>
                 </div>
             </div>
         </div>
         `
-            document.getElementById("cat-list-container").innerHTML += htmlContentToAppend;
+    }
+
+    document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(AUTOS).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            listadoAutos = resultObj.data
+            mostrarListadoAutos()
+            console.log(resultObj.data)
         }
-    })    
+    })
+})
+
+// nuevo codigo utilizando el getJSONData //
