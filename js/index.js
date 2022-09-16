4cbe082e232d207799e9e660571aa175e9fc0807
w4kemeup <110257712+w4kemeup@users.noreply.github.com>
2022-09-16 19:46:49 -0300

    update menu dropdown para ir a carrito, perfil y cerrar sesion

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+    document.getElementById("signoutBtn").addEventListener("click", function () {
+        localStorage.removeItem("correo");
+        window.location = "index.html"
+    })
 });
